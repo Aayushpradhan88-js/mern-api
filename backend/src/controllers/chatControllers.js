aa84d44e3e9cdb51adf95d50727c0d82a34a953f
Ayush Pradhan <AAYUSHPRADHAN996@outlook.com>
2025-05-17 13:05:42 +0545

    chat controller

--- backend/src/controllers/chatControllers.js
+++ backend/src/controllers/chatControllers.js
@@ -1,6 +1,7 @@
 import app from "../app.js";
 import http from 'http'
 import socketIo from 'socket.io'
+import { Chat } from "../models/chatModels.js";
 
 const server = http.createServer(app)
 const io = socketIo(server)
@@ -8,6 +9,10 @@ const io = socketIo(server)
 io.on('connection', (socket)=> {
 
   socket.on('sendMessage', async(message) => {
-    
+    const chat = new Chat(
+        {
+            userId: socket.handshake.auth.userId, message
+        }
+    )
   })
 })
