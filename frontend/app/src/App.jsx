88f78f4d5270856d6a342a1944fde5355ca3d5ae
Ayush Pradhan <AAYUSHPRADHAN996@outlook.com>
2025-05-20 16:53:05 +0545

    building ui and adding logics

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,12 +1,16 @@
+//underdevelopment and testing
 // import Login from "./pages/Login"
-import Main from "./pages/main"
+// import Main from "./pages/main"
 // import Register from "./pages/register"
-import VoiceSerch from "./components/voices/VoiceSerch"
-
+// import VoiceSerch from "./components/voices/VoiceSerch"
+// import { UI } from "./components/chat/ui"
+import CodeReview from "./components/assistant/CodeReview"
 const App = () => {
   return (
     <div>
-      <VoiceSerch/>
+      <CodeReview/>
+      {/* <UI/> */}
+      {/* <VoiceSerch/> */}
       {/* <Register /> */}
       {/* <Main /> */}
       {/* <Login /> */}
