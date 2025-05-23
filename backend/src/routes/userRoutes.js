bea50f57883989d95ae9d376259e383f161172ed
Ayush Pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2025-05-23 20:23:44 +0000

    setup complete: port and database connection complete

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -12,12 +12,32 @@ import { voiceSearch } from "../controllers/voiceSearchController.js";
 // import { aiController } from "../controllers/aiController.js";
 // import { assistanceController } from "../controllers/assistanceController.js";
 
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 const router = express.Router();
 
 router.post("/register",registerUser);
 
 router.post("/login", loginUser);
-router.post("/upload", upload.single("coverImage"), upload);
+router.post("/upload", upload.single("coverImage"), (req, res) => {
+  if(!req.file) {
+    throw new ApiError(400, "File is not uploaded")
+  }
+
+  console.log("File uploaded: ", req.file)
+
+  return (
+    res.status(200)
+    .json(
+      {
+        message: "File Uploaded Successfully",
+        Filename: req.Filename,
+        Filepath: req.Filepath
+      }
+    )
+  )
+});
 router.get("/search", voiceSearch);
 // router.get("/ai-response", aiController);
 // router.post("/assistance", assistanceController);
