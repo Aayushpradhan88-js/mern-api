fe8d5aa21f4dd321a4619a49c713fdf2d244e500
Ayush Pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2025-06-09 11:52:40 +0000

    writing multer configuration code

--- backend/src/middlewares/multerMiddleware.js
+++ backend/src/middlewares/multerMiddleware.js
@@ -2,7 +2,11 @@ import multer from 'multer'
 
 
 const storage = multer.diskStorage({
-    filename: function (req, file, cb) {
+    // TODO
+    // destination: function(req, file, cb) {
+    //     cb(null, './upload')
+    // },
+        filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
 })
