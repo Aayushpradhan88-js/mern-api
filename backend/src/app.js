e9bc80841deb8b2268cbc1b481b74504e15a7e93
Ayush Pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2025-06-09 09:41:37 +0000

    handling files

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 import cors from "cors";
 import express from "express";
 import { router as userRoutes } from "./routes/userRoutes.js";
+import {router as uploadRoutes} from "./routes/uploadRoutes.js";
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "16kb" }));
 
 app.use("/api", userRoutes);
-
+app.use("/upload", uploadRoutes);
 
 app.get("/", (req, res) => {
     res.send("Welcome to Backend")
