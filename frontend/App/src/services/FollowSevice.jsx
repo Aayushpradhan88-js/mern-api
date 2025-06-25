4ecbf81632f8d88a4e456445be4abffe16ed115e
Ayush Pradhan <AAYUSHPRADHAN996@outlook.com>
2025-06-25 17:55:36 +0545

    fetching backend data: processing

---
+++ frontend/App/src/services/FollowSevice.jsx
@@ -0,0 +1,30 @@
+import { Toast } from "../components/toast/Toast";
+
+export const ToogleFollowChannel = async (channelId, token) => {
+   try {
+     const response = await fetch(`http://localhost:4000/user/follow/${channelId}`, {
+         method: 'PATCH',
+         headers: {
+             "Content-Type": "application/json",
+             "Authorization": `Bearer ${localStorage.getItem("token")}`
+         }
+     });
+ 
+     if(response.ok) {
+         const errorResult = await response.json();
+         if(errorResult.ok) {
+             Toast.success(errorResult.json());
+         }
+ 
+         else {
+             Toast.error(errorResult.json())
+         }
+     }
+ 
+     const data = await response.json;
+     return data;
+ }
+   } catch (error) {
+    
+   }
+}
