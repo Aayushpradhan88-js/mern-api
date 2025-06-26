21943077372d8f9c1de076a9a3a90bd16d2f6be0
Ayush Pradhan <AAYUSHPRADHAN996@outlook.com>
2025-06-27 04:43:58 +0545

    add JWT verification to login route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -14,7 +14,7 @@ dotenv.config();
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/login", verifyJWT,loginUser);
 router.post("/logout", verifyJWT, logoutUser);
 router.patch("/follow/:channelId", verifyJWT, toogleFollow)
 
